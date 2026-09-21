59e115f4c92524301a325fbb992b5518b509d6d7
Carcara007 <vitoraugustosouzas007@gmail.com>
2026-09-21 15:01:10 -0300

    Criacao de todos os arquivos

--- SecureLoginPuc/src/main/java/com/example/SecureLoginPuc/application/SecureLoginPucApplication.java
+++ SecureLoginPuc/src/main/java/com/example/SecureLoginPuc/application/SecureLoginPucApplication.java
@@ -1,9 +1,9 @@
-package com.example.SecureLoginPuc;
+package com.example.SecureLoginPuc.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
 public class SecureLoginPucApplication {
 
 	public static void main(String[] args) {
